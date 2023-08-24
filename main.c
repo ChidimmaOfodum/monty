017fa5ccf15d2059a47cd5c71e9eef272d98fd09
Chidimma Ofodum <chidimmamofodum@gmail.com>
2023-08-24 01:41:21 -0700

    entry point for program

---
+++ main.c
@@ -0,0 +1,51 @@
+#include "monty.h"
+
+char *data;
+
+/**
+ * main - Entry point of program
+ * @ac: argument count
+ * @av: argument vector
+ * Return: success 0
+ */
+
+int main(int ac, char **av)
+{
+	size_t n = 0;
+	FILE *o = NULL;
+	char *op_name, *op_data, *lineptr = NULL, *delim = " \n\t\r";
+	int line_number = 0;
+	stack_t *stack = NULL;
+
+
+	if (ac != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+
+	o = fopen(av[1], "r");
+	if (!o)
+	{
+		fprintf(stderr, "Error: Can't open file <%s>\n", av[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	while (getline(&lineptr, &n, o) != -1)
+	{
+		line_number++;
+
+		op_name = strtok(lineptr, delim);
+		op_data = strtok(NULL, delim);
+
+		if (op_name)
+		{
+			data = op_data;
+			execute_op(op_name, &stack, line_number);
+
+		}
+	}
+
+	return (0);
+}
+
