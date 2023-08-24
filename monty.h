d6083a78042450dfcd85e56e92df67d922cfca8f
Chidimma Ofodum <chidimmamofodum@gmail.com>
2023-08-24 01:27:14 -0700

    updated header file

--- monty.h
+++ monty.h
@@ -6,6 +6,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
+
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -36,4 +37,10 @@ typedef struct instruction_s
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
+
+extern char *data;
+void pall(stack_t **stack, unsigned int line_number);
+void push(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
+void execute_op(char *op_name, stack_t **stack, unsigned int line_number);
 #endif /** MONTY_H **/
