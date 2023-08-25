2a40073b6f2db6b80e88e8e028dbfe10f966f6fe
Vulcanric <jneric49@gmail.com>
2023-08-24 19:33:54 -0700

    fix: added newline to stderr output

--- op_func.c
+++ op_func.c
@@ -82,7 +82,7 @@ void pint(stack_t **stack, unsigned int line_number)
 	if (*stack == NULL)
 	{
 		n = line_number;
-		fprintf(stderr, "L%d: can't pint, stack empty", n);
+		fprintf(stderr, "L%d: can't pint, stack empty\n", n);
 		fclose(stream.o);
 		freeStack(*stack);
 		exit(EXIT_FAILURE);
