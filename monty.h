a86b861d1b242fcc0fe0e2ce5415f86489cc0ed1
Vulcanric <jneric49@gmail.com>
2023-08-26 06:39:29 -0700

    update: added prototypes of newly created functions

--- monty.h
+++ monty.h
@@ -64,6 +64,11 @@ void add(stack_t **stack, unsigned int line_number);
 void sub(stack_t **stack, unsigned int line_number);
 /* changed the name, because there's a standard div function */
 void divid(stack_t **stack, unsigned int line_number);
+void mul(stack_t **stack, unsigned int line_number);
+void mod(stack_t **stack, unsigned int line_number);
+void pchar(stack_t **stack, unsigned int line_number);
+void pstr(stack_t **stack, unsigned int line_number);
+void rotl(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
 void freeStack(stack_t *stack);
 int isInteger(char *data);
