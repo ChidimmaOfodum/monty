d5ec86e468b2d9eccbf7db4508de6de715db07b8
jneric49@gmail.com <jneric49@gmail.com>
2023-08-26 22:25:17 +0100

    merge branches
    fixed merge conflict

---
+++ bytecodes/mul.m
@@ -0,0 +1,9 @@
+push 3
+push 3
+push 5
+pall
+push 32
+pchar
+pop
+mul
+pall
---
+++ bytecodes/rotr.m
@@ -0,0 +1,16 @@
+push 0
+push 9
+push 8
+push 7
+push 6
+push 5
+push 4
+push 3
+push 2
+push 1
+pall
+push 32
+pchar
+pop
+rotr
+pall
